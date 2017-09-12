09dc6c03a8e8462d36769e7d662b91c47c8565c1
Furiously Curious <FuriouslyCurious@users.noreply.github.com>
2017-09-12 14:56:31 -0500

    Pending HIPAA Audit for Patient Privacy

--- DLCode/Where_is_the_code.py
+++ DLCode/Where_is_the_code.py
@@ -1,7 +1,7 @@
 """ 
-    AiAi.care uses confidential Patient X-Rays protected by HIPAA. 
-    We we will publish the code on public Github repo after an audit to make sure 
-    no HIPAA compliant information is published accidentally.
+    AiAi.care uses confidential Patient X-Ray data protected by HIPAA. 
+    We we will publish the code on Github Public repo after an audit to make sure 
+    no HIPAA / PII information is published accidentally.
 
-    In the meantime we are working off of a private repository.
+    In the meantime we are working off of a private repository.  Sorry 😔
 """"
