32ee7746abea885a5fe1b4804f71f81a8bb4a6f3
Furiously Curious <FuriouslyCurious@users.noreply.github.com>
2017-10-20 19:31:04 -0400

    Update Where_is_the_code.py

--- DLCode/Where_is_the_code.py
+++ DLCode/Where_is_the_code.py
@@ -1,6 +1,6 @@
 """ 
     AiAi.care uses confidential Patient X-Ray data protected by HIPAA. 
-    We we will publish the code on Github Public repo after an audit to make sure 
+    We we will publish the code on Github Public repo after a code-audit to make sure 
     no HIPAA / PII information is published accidentally.
 
     In the meantime we are working off of a private repository.  Sorry 😔
